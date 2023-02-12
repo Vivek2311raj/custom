a7f91cc3540de30068d8bea68181565a6cfa6bd0
Vivek <vivek2311raj@gmail.com>
2023-02-12 18:09:45 +0530

    first commit

---
+++ src/useToggle.js
@@ -0,0 +1,10 @@
+import React, { useCallback, useState } from "react";
+
+const useToggle = () => {
+  const [state, setState] = useState(false);
+
+  const toggle = () => setState((prevState) => !prevState);
+
+  return [state, toggle];
+};
+export default useToggle;
